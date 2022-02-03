6887466e4db1bc0cccc77f4fdc1e6d34b86a9770
mg911004 <mg911003@naver.com>
2022-02-03 16:54:56 +0900

    baseurl 변경

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,8 +8,7 @@ export default {
     },
     env: {
         listNumber : 15, //한 화면에 보여줄 데이터 수
-        //nodeURL : 'http://54.210.225.118:3001/'
-        nodeURL : "http://commu.cafe24app.com:8001/"
+        nodeURL : 'http://54.210.225.118:8001/'
     },
     // Global page headers: https://go.nuxtjs.dev/config-head
     // <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
@@ -57,8 +56,7 @@ export default {
         '@nuxtjs/axios'
     ],
     axios: {
-        //baseURL: 'http://54.210.225.118:3001/',
-        baseURL : "http://commu.cafe24app.com:8001/"
+        baseURL: 'http://54.210.225.118:8001/',
     },
     // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
     vuetify: {
